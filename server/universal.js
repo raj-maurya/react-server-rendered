0f7cc731730d326ee9d1e3a9c5e1e43bc81e9552
Danilo Lemes <daniloleemes@users.noreply.github.com>
2017-08-23 23:25:31 -0300

    initial

--- server/universal.js
+++ server/universal.js
@@ -54,7 +54,9 @@ module.exports = function universalLoader(req, res) {
                 <title>${filme.title} - Sugestão Netflix</title>
                 <meta property="og:type" content="video.other">
                 <meta property="og:image" content="${filme.poster}">
-                <meta property="og:title" content="${filme.title}">
+                <meta property="og:image:height" content="268">
+                <meta property="og:image:width" content="182">
+                <meta property="og:title" content="Sugestão aleatória do dia: ${filme.title} - ${filme.year}">
                 <meta property="og:url" content="http://34.209.188.46:8080/${param}">
                 <meta property="og:description" content="${filme.description}">
               </head>
